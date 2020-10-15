bcc1c28b47a89f3ab99e893ee1e1d256d212f681
Rohan2596 <rohankadam662@gmail.com>
2020-10-15 14:21:30 +0530

    ADDED:Player Gets to exact winning position.

--- main/java/SnakeLadderSimulation.java
+++ main/java/SnakeLadderSimulation.java
@@ -63,10 +63,22 @@ public class SnakeLadderSimulation {
         }
     }
 
+    /*
+     * @author  : ROHAN KADAM
+     * @purpose : To play till the Player reaches the winning position 100.
+     * @date    : 15/10/2020
+     * */
+    public void tillWinningPosition(){
+        if(position>END_POSITION){
+            position=position-rollDiceValue;
+            playerPlaysTillWinning();
+        }
+    }
 
     public static void main(String[] args) {
         SnakeLadderSimulation snakeLadderSimulation = new SnakeLadderSimulation();
         snakeLadderSimulation.playerPlaysTillWinning();
+        snakeLadderSimulation.tillWinningPosition();
 
     }
 }
