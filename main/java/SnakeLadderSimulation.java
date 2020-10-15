e2784fe74eb5c162474385b7f8621dc1763e2b73
Rohan2596 <rohankadam662@gmail.com>
2020-10-15 10:26:48 +0530

    ADDED:Player Rolls Dice to get Number Between 1 to 6.

--- main/java/SnakeLadderSimulation.java
+++ main/java/SnakeLadderSimulation.java
@@ -1,10 +1,28 @@
+import java.util.Random;
+
 public class SnakeLadderSimulation {
 
+    /*
+    * Constant
+    * */
     public static final int START_POSITION=0;
-    public static final int PLAYER_ONE=0;
+    public static final int PLAYER_ONE=1;
     public static final int END_POSITION=100;
 
-    public static void main(String[] args) {
+    /*
+    * Variables
+    * */
+    int rollDiceValue;
 
+
+    public  void rollDice(){
+        Random random = new Random();
+        rollDiceValue= random.nextInt(6) +PLAYER_ONE ;
+
+    }
+
+    public static void main(String[] args) {
+        SnakeLadderSimulation snakeLadderSimulation=new SnakeLadderSimulation();
+          snakeLadderSimulation.rollDice();
     }
 }
