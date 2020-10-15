1e06f3170be405fccb6e9e7bd85a31671b08149f
Rohan2596 <rohankadam662@gmail.com>
2020-10-15 14:43:38 +0530

    REMOVED:Dice Count.

--- main/java/SnakeLadderSimulation.java
+++ main/java/SnakeLadderSimulation.java
@@ -26,6 +26,7 @@ public class SnakeLadderSimulation {
         Random random = new Random();
         rollDiceValue = random.nextInt(6) + PLAYER_ONE;
 
+
     }
     /*
      * @author  : ROHAN KADAM
