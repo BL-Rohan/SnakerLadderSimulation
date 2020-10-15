e86efcd08c778d6d772b85f69df70b46d49ab376
Rohan2596 <rohankadam662@gmail.com>
2020-10-15 10:02:59 +0530

    ADDED:Single Player and start_position at 0.

--- main/java/SnakeLadderSimulation.java
+++ main/java/SnakeLadderSimulation.java
@@ -1,5 +1,9 @@
 public class SnakeLadderSimulation {
 
+    public static final int START_POSITION=0;
+    public static final int PLAYER_ONE=0;
+    public static final int END_POSITION=100;
+
     public static void main(String[] args) {
 
     }
